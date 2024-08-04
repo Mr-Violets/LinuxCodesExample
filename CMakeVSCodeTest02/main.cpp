9a24631e768bf9ee5f97179375684a4c8492e64b
Violets Purple <clee2019_v@hotmail.com>
2024-08-04 23:28:59 +0800

    2024.08.04 23:28

---
+++ CMakeVSCodeTest02/main.cpp
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+
+// int main(int, char**){
+//     printf("Hello, from MinGWGCC!\n");
+// }
+
+#include <iostream>
+using namespace std;
+
+int main(int agec, char **argv) {
+    cout << "hello word ,form vscode cmake" << endl;
+}
+
